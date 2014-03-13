c1af0802212879bf4ce400d2fe93040f65a8718f
Xali Beut <xalibeut@yahoo.ca>
2014-03-13 12:09:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SunudemoApp::Application.config.secret_key_base = '3cabd0dd6610b1e0d06d42d9ddeaacc4e2e0a1ec2e3d87b4ff211f31f4fb2a0532d1e264f9138537acf008e8d7703e61d1e4e151083a348b664e46b85613a744'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SunudemoApp::Application.config.session_store :cookie_store, key: '_sunudemo-app_session'
